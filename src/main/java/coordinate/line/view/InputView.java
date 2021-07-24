1901bdd7f0af43a2d0a167d3931784dda00a54c2
Jiyeon Rhea Park <rheazen@gmail.com>
2021-07-25 03:09:25 +0900

    FEAT ask re input if error

--- src/main/java/coordinate/line/view/InputView.java
+++ src/main/java/coordinate/line/view/InputView.java
@@ -8,7 +8,7 @@ public class InputView {
 
     InputView(){}
 
-    public static final BeCoordinates askInput() throws Exception {
+    public static final BeCoordinates askInput() {
         System.out.print("좌표를 입력하세요.");
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
@@ -20,7 +20,9 @@ public class InputView {
             return new BeDoubleCoord(new BeCoordinate(inputs[0], inputs[1]),
                     new BeCoordinate(inputs[2], inputs[3]));
         }
-        throw new IllegalArgumentException("2개 또는 4개 입력하세요");
+        System.out.println("2개 또는 4개 입력하세요");
+        BeCoordinates beCoordinates = askInput();
+        return beCoordinates;
     }
 
 }
