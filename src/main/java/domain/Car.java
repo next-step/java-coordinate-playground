8cb6b8d8790324ed3ec59d73d8a5455c88c400fd
akskflwn <akskflwn@gmail.com>
2022-10-06 03:40:28 +0900

    feat : 자동차 이름 검증 구현

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -3,16 +3,35 @@ package domain;
 public class Car {
     private static final int STARTING_POSITION = 0;
     private static final int MOVE_LIMIT = 4;
+    private static final String NAME_ARGUMENTS = "[Error] 자동차 이름은 1자 이상, 5자 이하여야 합니다.";
     private String name;
     private int position;
 
-    public Car(String name) {
+    public Car(String name) throws IllegalArgumentException{
+        validate(name);
         this.name = name;
         this.position = STARTING_POSITION;
     }
 
+    private void validate(String name) {
+        isValidLength(name);
+        isNotEmpty(name);
+    }
+
+    private void isNotEmpty(String name) {
+        if (name.trim().isEmpty()) {
+            throw new IllegalArgumentException(NAME_ARGUMENTS);
+        }
+    }
+
+    private void isValidLength(String name) {
+        if (name.length() >= 5) {
+            throw new IllegalArgumentException(NAME_ARGUMENTS);
+        }
+    }
+
     public void move(int number) {
-        if(isMovable(number)) {
+        if (isMovable(number)) {
             position++;
         }
     }
@@ -21,7 +40,7 @@ public class Car {
         return number >= MOVE_LIMIT;
     }
 
-    public int getPosition(){
+    public int getPosition() {
         return this.position;
     }
 }
--- src/test/java/CarTest.java
+++ src/test/java/CarTest.java
@@ -1,8 +1,11 @@
 import domain.Car;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class CarTest {
 
@@ -26,5 +29,14 @@ public class CarTest {
 
         //then
         assertThat(car.getPosition()).isEqualTo(0);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", "longName"})
+    public void 이름_길이_테스트(String validName){
+        assertThatThrownBy(()->
+            new Car(validName)
+        ).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[Error] 자동차 이름은 1자 이상, 5자 이하여야 합니다.");
     }
 }
